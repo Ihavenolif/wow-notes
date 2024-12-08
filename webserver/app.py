0e80fcabaebd45ca11d15bd045d20587db0c8558
ihavenolif <matuswall@gmail.com>
2024-12-08 18:57:51 +0100

    added redirect

--- webserver/app.py
+++ webserver/app.py
@@ -1,10 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    return redirect("/court2")
     return "Hello, World!"
 
 @app.route("/court")
